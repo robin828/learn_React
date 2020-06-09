1580870e60590ef55f495aa1bf44959b1e3c1b6f
robin19093 <robin19093@gmail.com>
2020-06-10 01:21:03 +0530

    First App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class App extends Component{
+    render(){
+        return <div>
+                    <h1>HI HELLO HOW ARE YOU</h1>
+                </div>
+    }
+}
+export default App;
