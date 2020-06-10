27a34680e88e41c13979c1f77b61f6944d12cfa0
robin828 <robin19093@iiitd.ac.in>
2020-06-10 11:14:14 +0530

    PropType

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, {Component, Fragment} from "react";
-
-class App extends Component{
+import PropTypes from "prop-types";
+class Apps extends Component{
     render(){
         return <Fragment>
-                    <h1>hi hello</h1>
+
+                    <h1>hi hello abey chal</h1>
                     <h1>how are you</h1>
                     <h1>Tell about yourself</h1>
                 </Fragment>
     }
 }
-export default App;
+
+const Student = (props) => {
+    return <>
+                <h1>hi hello {props.name}</h1>
+                <h1>hi {props.clas}</h1>
+                <h1>hi {props.lang}</h1>
+            </>
+}
+
+const Teacher = (props) => {
+    return <>
+                <Apps/>
+                <Student name="Robin" clas="twelth" lang="english"/>
+                <h1>hi Teacher {props.name}</h1>
+                <h1>hi {props.clas}</h1>
+                <h1>hi {props.lang}</h1>
+            </>
+}
+Teacher.propTypes = {
+    name: PropTypes.string,
+    lang: PropTypes.string,
+    clas: PropTypes.string
+};
+
+ 
+export default Teacher;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Teacher from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(<Teacher name="vikas" clas="two" lang="hindi"/>, document.getElementById("root"));
 
