2d3b12f4b700f59a0e80b10bbbcbabb068e7c046
robin828 <robin19093@iiitd.ac.in>
2020-06-10 09:14:04 +0530

    Fragment Part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 
 class App extends Component{
     render(){
-        return <div>
-                    <h1>HI HELLO HOW ARE YOU</h1>
-                </div>
+        return <Fragment>
+                    <h1>hi hello</h1>
+                    <h1>how are you</h1>
+                    <h1>Tell about yourself</h1>
+                </Fragment>
     }
 }
 export default App;
